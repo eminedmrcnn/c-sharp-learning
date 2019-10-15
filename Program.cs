7dd76f2ad1276924a148a11f5e780caa684c1491
eminedmrcn <eminemecem@gmail.com>
2019-10-15 20:03:10 +0300

    metod ile kullanıcan veri alarak toplama yaptım (YAPTIIIIMMM)

--- Program.cs
+++ Program.cs
@@ -4,65 +4,58 @@ namespace Döngüler14_10_19
 {
 	class Program
 	{
-		
-			//static void yazdir(string bilgi)
-			//{
-			//	for (int i = 0; i < 10; i++)
-			//		Console.WriteLine(bilgi);
-			//}
-			static int topla(int sayi1, params int[] sayilar)
-			{
-				int toplam = 0;
-				for (int i = 0; i < sayilar.Length; i++)
-				{
-					toplam += sayilar[i];  //toplam=toplam+sayilar[i];
-				}
-				return toplam;
 
+		static int topla(int sayi1, params int[] sayilar)
+		{
+			int toplam = 0;
+			for (int i = 0; i < sayilar.Length; i++)
+			{
+				toplam += sayilar[i];  //toplam=toplam+sayilar[i];
 			}
+			return toplam;
+
+		}
 
 
 
 
-			static void Main(string[] args)
+		static void Main(string[] args)
+		{
+
+			int i, sayi, deger;
+			double toplam = 0;
+			Console.WriteLine("kaç sayi gireceksiniz");
+			deger = Convert.ToInt32(Console.ReadLine());
+			for (i = 1; i <= deger; i++)
 			{
-				int sayi;
-				Console.WriteLine("kaç sayi:");
+				Console.WriteLine(i + ".sayiyi giriniz...");
 				sayi = Convert.ToInt32(Console.ReadLine());
-
-				int[] sayilar1 = new int[sayi - 1];
-
-
-				for (int i = 0; i < sayi;)
-				{
-					Console.WriteLine("sayiları giriniz:");
-
-					sayilar1[i] = Convert.ToInt32(Console.ReadLine());
-					i++;
-
-				}
-				topla(sayi, sayilar1);
-
-
-
-				Console.ReadLine();
-
-
-
-
-
-
-
-
-				//Console.WriteLine("bi metin yaziniz:");
-				//string bilgi = Console.ReadLine();
-				//yazdir(bilgi);
-				//Console.Read();
-
-
+				toplam += sayi;
 
 			}
 
-		
+
+			Console.WriteLine(toplam);
+
+
+
+			Console.ReadLine();
+
+
+
+
+
+
+
+
+			//Console.WriteLine("bi metin yaziniz:");
+			//string bilgi = Console.ReadLine();
+			//yazdir(bilgi);
+			//Console.Read();
+
+
+
+		}
+
 	}
 }
