6beb4835cfd69d42d8078cda90169ee2fa80f81b
eminedmrcn <eminemecem@gmail.com>
2019-10-15 12:44:16 +0300

    dosyaların githuba ilk defa yuklenmesi

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Döngüler14_10_19
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//int i;
+			//for (i=0;i<100;i+=2)
+			//{
+			//	Console.WriteLine(i);
+			//}
+			//Console.WriteLine("bitis");
+			//Console.ReadLine();
+
+
+
+			//int sayi=100;
+			//while (sayi >= 0)
+			//{
+			//	Console.WriteLine(sayi);
+			//	sayi--;
+
+			//}
+			//Console.WriteLine("bitis {0}",sayi);
+			//Console.ReadLine();
+
+
+
+			//int sayi = 10;
+			//do 
+			//{
+			//	Console.WriteLine(sayi);
+			//	sayi--;
+			//} while (sayi>=0);
+
+
+			string[] ögrenciler = new string[3] { "ahmet", "mehmet", "emine" };
+			foreach(var ögrenci in ögrenciler)
+			{
+				Console.WriteLine(ögrenci);
+			}
+			Console.ReadLine();
+
+		}
+	}
+}
