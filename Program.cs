c6a6c079dd56d45b43263c9c273c8230fcb63392
eminedmrcn <eminemecem@gmail.com>
2019-10-15 19:51:41 +0300

    gülşahla birlikte metodlarda kullanıcıdan veri almaya çalıştık

--- Program.cs
+++ Program.cs
@@ -4,45 +4,65 @@ namespace Döngüler14_10_19
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-			//int i;
-			//for (i=0;i<100;i+=2)
+		
+			//static void yazdir(string bilgi)
 			//{
-			//	Console.WriteLine(i);
+			//	for (int i = 0; i < 10; i++)
+			//		Console.WriteLine(bilgi);
 			//}
-			//Console.WriteLine("bitis");
-			//Console.ReadLine();
-
-
-
-			//int sayi=100;
-			//while (sayi >= 0)
-			//{
-			//	Console.WriteLine(sayi);
-			//	sayi--;
-
-			//}
-			//Console.WriteLine("bitis {0}",sayi);
-			//Console.ReadLine();
-
-
-
-			//int sayi = 10;
-			//do 
-			//{
-			//	Console.WriteLine(sayi);
-			//	sayi--;
-			//} while (sayi>=0);
-
-
-			string[] ögrenciler = new string[3] { "ahmet", "mehmet", "emine" };
-			foreach(var ögrenci in ögrenciler)
+			static int topla(int sayi1, params int[] sayilar)
 			{
-				Console.WriteLine(ögrenci);
-			}
-			Console.ReadLine();
+				int toplam = 0;
+				for (int i = 0; i < sayilar.Length; i++)
+				{
+					toplam += sayilar[i];  //toplam=toplam+sayilar[i];
+				}
+				return toplam;
 
-		}
+			}
+
+
+
+
+			static void Main(string[] args)
+			{
+				int sayi;
+				Console.WriteLine("kaç sayi:");
+				sayi = Convert.ToInt32(Console.ReadLine());
+
+				int[] sayilar1 = new int[sayi - 1];
+
+
+				for (int i = 0; i < sayi;)
+				{
+					Console.WriteLine("sayiları giriniz:");
+
+					sayilar1[i] = Convert.ToInt32(Console.ReadLine());
+					i++;
+
+				}
+				topla(sayi, sayilar1);
+
+
+
+				Console.ReadLine();
+
+
+
+
+
+
+
+
+				//Console.WriteLine("bi metin yaziniz:");
+				//string bilgi = Console.ReadLine();
+				//yazdir(bilgi);
+				//Console.Read();
+
+
+
+			}
+
+		
 	}
 }
