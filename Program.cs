f3c29d421b457b2545cbc4bec818d8566aef6525
eminedmrcn <eminemecem@gmail.com>
2019-10-19 10:25:07 +0300

    string ile çalıştım.

--- Program.cs
+++ Program.cs
@@ -3,50 +3,27 @@
 namespace Döngüler14_10_19
 {
 	class Program
-	{
-
-		
-
-
-
-
-		static void Main(string[] args)
+	{static void Main(string[] args)
 		{
-			int sayi;
-
-			Console.WriteLine("bi sayi giriniz:");
-			sayi = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine("..........");
-
-			for(int i=2;i<=sayi;i++)
-			{
-				int kalan;
-				kalan = sayi % i;
-				if (kalan == 0)
-				{
-					Console.WriteLine("bu sayi asal deildir.");
-					break;
-				}
-
-				else if (i == sayi - 1)
-				{
-					Console.WriteLine("sayi asaldir.");
-					break;
-				}
 
 
-			}
-			
+
+
+			string cümle = "benim adım emine";
+			var result = cümle.Length;  //kaç karakter girdiğimizi belirtir.
+			var result2 = cümle.Clone(); //klonluyo..
+			cümle = "benim adım emine";
+			Console.WriteLine(result);
+			bool result3 = cümle.EndsWith("e"); //cümle e ile bitiyor mu..
+			bool result4 = cümle.StartsWith("b"); //cümle b ile başlıyor mu..
+			var result5 = cümle.IndexOf("adım");   //adım keilmesinin kaçıncı karakterde yazıldığını söyler..
+
+			Console.WriteLine(result2);
+			Console.WriteLine(result3);
+			Console.WriteLine(result4);
+			Console.WriteLine(result5);
+
 			Console.ReadLine();
-
-
-
-
-
-
-
-
-
 
 
 
@@ -56,7 +33,27 @@ namespace Döngüler14_10_19
 
 		}
 
-		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
