cee6ad96d8758cfa3fe6a58cddfca04e40151c8a
eminedmrcn <eminemecem@gmail.com>
2019-10-21 01:55:44 +0300

    başlangıç seviyesinde interface öğrendim..

--- Program.cs
+++ Program.cs
@@ -5,28 +5,63 @@ namespace Döngüler14_10_19
 	class Program
 	{static void Main(string[] args)
 		{
-			MüsteriMüdürü müsteriMüdürü = new MüsteriMüdürü();
-			müsteriMüdürü.Ekle();
-			müsteriMüdürü.Güncelleme();
+			PersonManager manager = new PersonManager();
+			Customer customer = new Customer
+			{
+				Id = 1,
+				FirstName = "Emine",
+				LastName = "Demircan",
+				Adress = "İstanbul"
+			};
 
-			Müsteri müsteri = new Müsteri();
-			müsteri.Adi = "Emine";
-			müsteri.Id = 1;
-			müsteri.Soyadi = "Demircan";
-			müsteri.Sehri = "İstanbul";
-			
-
-			Console.WriteLine(müsteri.Adi);
-			Console.WriteLine(müsteri.Soyadi);
-			Console.WriteLine(müsteri.Sehri);
-				
-
-
+			Student student = new Student
+			{
+				Id = 1,
+				FirstName = "Mehmet",
+				LastName="Yeşilmen",
+				Departman="Computer Sciences"
 
 
+
+			};
+			manager.Add(customer);
+			manager.Add(student);
 			Console.ReadLine();
 		}
 
+		interface IPerson
+		{
+			int Id { get; set; }
+			string FirstName { get;  set; }
+			string LastName { get; set; }
+			 
+			
+		}
+
+		class Customer : IPerson
+		{
+			public int Id { get; set; }
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+
+			public string Adress { get; set; }
+		}
+
+		class Student : IPerson
+		{
+			public int Id { get; set; }
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public string Departman { get; set; }
+		}
+
+		class PersonManager
+		{
+			public void Add(IPerson persom)
+			{
+				Console.WriteLine(persom.FirstName);
+			}
+		}
 		
 			
 
