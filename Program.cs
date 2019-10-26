125326cce0d85d1410d48b03b4673e1d7c056263
eminedmrcn <eminemecem@gmail.com>
2019-10-26 14:16:59 +0300

    nesne örnekleri için constructor

--- Program.cs
+++ Program.cs
@@ -9,24 +9,64 @@ namespace Döngüler14_10_19
 		{
 			CustomerManager customerManager = new CustomerManager(20);
 			customerManager.List();
+			Product product = new Product()
+			{
+				Id = 1,
+				Name = "Laptop"
+			};
+			Product product2 = new Product(2, "Computer");
+			
+
+
+			Console.ReadLine();
+			
 		}
 	}
 
 	class CustomerManager
 	{
-		private int _count;
+		private int _count = 15;
 		public CustomerManager(int count)
 		{
 			_count = count;
 		}
+
+		public CustomerManager()
+		{
+
+		}
+		
 		public  void List()
 		{
-			Console.WriteLine("Listed! {0},items", _count);
+			Console.WriteLine("Listed! {0} items", _count);
 		}
 		public void Add()
 		{
 			Console.WriteLine("Added!");
 		}
+	}
+
+	class Product
+	{
+
+		public Product()
+		{
+
+		}
+		int _id;
+		string _name;
+		public Product(int id,string name)
+		
+		{
+			_id = id;
+			_name = name;
+		}
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		
+
+		
 	}
 
 	
