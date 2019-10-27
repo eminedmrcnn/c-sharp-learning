23d717d88b01dece0bdc026211a2a963708e2581
eminedmrcn <eminemecem@gmail.com>
2019-10-27 17:02:18 +0300

    Collection özellik ve metod

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Döngüler14_10_19
 {
@@ -8,24 +9,76 @@ namespace Döngüler14_10_19
 	{
 		static void Main(string[] args)
 		{
-			ArrayList cities = new ArrayList();
+
+			List<string> cities = new List<string>();
 			cities.Add("Ankara");
 			cities.Add("Adana");
+
+			Console.WriteLine(cities.Contains("Ankara"));  //Aramak istediğimizi contains içine yazarız..
+
 
 			foreach (var city in cities)
 			{
 				Console.WriteLine(city);
 			}
 
-			Console.WriteLine("İstanbul");
+			List<Customer> customers = new List<Customer>();
+			customers.Add(new Customer { Id = 1, FirstName = "emine" });
+			customers.Add(new Customer { Id = 2, FirstName = "Demircan" });
 
-			Console.WriteLine(cities[2]);
+			
+			var customer1 = new Customer
+			{
+				Id = 1, FirstName = "mahmut"
+			};
+
+			customers.Add(customer1);
+			customers.AddRange(new Customer[2]
+			{
+				new Customer
+				{
+					Id=4,
+					FirstName="Ali"
+				},
+				new Customer
+				{
+					Id=5,
+					FirstName="Ayşe"
+				},
+			});
+
+
+			Console.WriteLine(customers.Contains(customer1));
+
+			var index = customers.IndexOf(customer1);
+			Console.WriteLine("index..{0}", index);
+
+			customers.Insert(0, customer1);
+
+
+			//customers.Clear();
+
+			
+
+			foreach (var customer in customers)
+			{
+				Console.WriteLine(customer.FirstName);
+			}
+			var count = customers.Count;
+			Console.WriteLine("Count..{0}", count);
+
 
 
 			
 			Console.ReadLine();
 
 		}
+	}
+
+	class Customer
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
 	}
 
 	
